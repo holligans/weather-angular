1839d0b96e88743e71583cb30ae0f12a593377cd
M8 Admin <m8admin@M8s-iMac-2.local>
2018-04-27 17:02:10 -0400

    all components created along with the routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
 
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./header/header.component";
+import { EnterCityComponent } from "./enter-city/enter-city.component";
+import { WeekWeatherComponent } from "./week-weather/week-weather.component";
+import { WeekDailyComponent } from "./week-daily/week-daily.component";
+import { NotFoundComponent } from "./not-found/not-found.component";
 
-import { AppComponent } from './app.component';
-
-
+const appRoutes: Routes = [
+  { path: "", component: EnterCityComponent },
+  { path: "weekly", component: WeekWeatherComponent },
+  { path: "day/:id", component: WeekDailyComponent },
+  { path: "**", component: NotFoundComponent }
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    EnterCityComponent,
+    WeekWeatherComponent,
+    WeekDailyComponent,
+    NotFoundComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, RouterModule.forRoot(appRoutes)],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/week-daily/week-daily.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-week-daily',
+  templateUrl: './week-daily.component.html',
+  styleUrls: ['./week-daily.component.css']
+})
+export class WeekDailyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
